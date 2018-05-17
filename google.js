07c73bb83b6321cc8959063d550a885ad5987b72
IsaacReed <isaacreed758@gmail.com>
2018-05-17 11:13:53 +0300

    hi

--- google.js
+++ google.js
@@ -5,4 +5,6 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-gapi.load("auth2")
+gapi.load("auth2", function(){
+	gapi.auth2.init();
+});
