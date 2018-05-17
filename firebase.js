5ef683fb416fc186243bc5a44530983be49a2e34
IsaacReed <isaacreed758@gmail.com>
2018-05-17 11:09:22 +0300

    hi

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC-TboUSZEOJExecr5WsTl3rh6hpKwAnqQ",
+    authDomain: "chat-app-78a2c.firebaseapp.com",
+    databaseURL: "https://chat-app-78a2c.firebaseio.com",
+    projectId: "chat-app-78a2c",
+    storageBucket: "chat-app-78a2c.appspot.com",
+    messagingSenderId: "576770306956"
+  };
+  firebase.initializeApp(config);
