7f2748d0d5f0e691dc686fbc4f72b9fdd28477cd
IsaacReed <isaacreed758@gmail.com>
2018-06-05 14:10:05 +0300

    made app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+db.collection("users").add({
+   
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if request.auth.uid != null;
+    }
+  }
+}
