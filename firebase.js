59f7196acf9642a6c60057994832ab0f6fc5cb8b
BaileyFoot <email2baileyf@googlemail.com>
2018-06-08 00:06:24 +0300

    Updated Firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,13 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC-TboUSZEOJExecr5WsTl3rh6hpKwAnqQ",
-    authDomain: "chat-app-78a2c.firebaseapp.com",
-    databaseURL: "https://chat-app-78a2c.firebaseio.com",
-    projectId: "chat-app-78a2c",
-    storageBucket: "chat-app-78a2c.appspot.com",
-    messagingSenderId: "576770306956"
-  };
-  firebase.initializeApp(config);
+//<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+//<script>
+  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC-TboUSZEOJExecr5WsTl3rh6hpKwAnqQ",
+  authDomain: "chat-app-78a2c.firebaseapp.com",
+  databaseURL: "https://chat-app-78a2c.firebaseio.com",
+  projectId: "chat-app-78a2c",
+  storageBucket: "chat-app-78a2c.appspot.com",
+  messagingSenderId: "576770306956"
+};
+firebase.initializeApp(config);
+//</script>
